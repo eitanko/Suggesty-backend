b177a6e1cf06b7f23b4a93d79b2a184b11cc7e67
Eitan Koren <eitanko@gmail.com>
2025-01-11 19:42:49 +0200

    fixed step model

--- routes/step.py
+++ routes/step.py
@@ -61,7 +61,7 @@ def save_step():
             url=data["url"],
             event_type=data["eventType"],
             element=data["element"],
-            screen_path=screenshot_url,
+            screenshot_url=screenshot_url,
             index=data["index"]
         )
 
